3dda2f5383d2c806733d09c66ed840aed02dd80a
beven.zhou@hotmail.com <beven.zhou@hotmail.com>
2022-02-27 10:13:07 +0800

    update

--- build.sh
+++ build.sh
@@ -61,12 +61,12 @@ make install
 cd ../
 
 ARGS=(
-  src/decoder.c dist/ffmpeg/lib/libavcodec.a dist/ffmpeg/lib/libavutil.a
+  src/audio_decoder.c dist/ffmpeg/lib/libavcodec.a dist/ffmpeg/lib/libavutil.a
   -O3
   -I dist/ffmpeg/include
   -s WASM=1
   -s TOTAL_MEMORY=67108864
-  -s EXPORTED_FUNCTIONS="[_malloc,_free,_openDecoder,_flushDecoder,_closeDecoder,_decodeData,_main]"  # export main and proxy_main funcs
+  -s EXPORTED_FUNCTIONS="[decode]"  # export main and proxy_main funcs
   -s EXPORTED_RUNTIME_METHODS="[addFunction]"
   -s RESERVED_FUNCTION_POINTERS=14
   -s FORCE_FILESYSTEM=1
