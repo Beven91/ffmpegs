27c813ce45514f4288db23365132042b55d45715
beven.zhou@hotmail.com <beven.zhou@hotmail.com>
2022-06-03 00:20:09 +0800

    优化构建

--- apply.js
+++ apply.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 const file = './dist/src/asmlib.template.js';
-const assemblyAsmlib = require('./assembly/audio');
+const id = require.resolve('./dist/audio');
+const assemblyAsmlib = require(id);
 
-const code = assemblyAsmlib.toString().replace(/\n/g,' ').replace(/\\/g,'\\\\').replace(/'/g,"\\'");
+const code = assemblyAsmlib.toString().split(/\r\n|\n/).join(' ').replace(/\\/g,'\\\\').replace(/'/g,"\\'");
 fs.writeFileSync(file, `module.exports = '${code}'`);
+fs.unlinkSync(id);
